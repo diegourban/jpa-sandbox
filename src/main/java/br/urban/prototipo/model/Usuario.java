5a147532dcedfce4cb65e05fcb118c0aeb2fe916
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-06-28 21:11:22 -0300

    ajustes

--- src/main/java/br/urban/prototipo/model/Usuario.java
+++ src/main/java/br/urban/prototipo/model/Usuario.java
@@ -5,23 +5,20 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "USUARIO")
 public class Usuario {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private Long id;
 	
-	@Column
+	@Column(name = "login")
 	private String login;
-	
-	@Column
-	private String password;
-	
-	@Column
-	private String salt;
-	
+		
 	public Long getId() {
 		return id;
 	}
@@ -36,22 +33,6 @@ public class Usuario {
 	
 	public void setLogin(String login) {
 		this.login = login;
-	}
-	
-	public String getPassword() {
-		return password;
-	}
-	
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	public String getSalt() {
-		return salt;
-	}
-	
-	public void setSalt(String salt) {
-		this.salt = salt;
 	}
 
 }
