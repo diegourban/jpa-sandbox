636d906f85a6b36386673ea0090188bd9d4a76a9
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-09-02 16:24:24 -0300

    enabling hibernate statistics

---
+++ src/test/java/br/com/urban/sandbox/jpa/TesteStatistics.java
@@ -0,0 +1,8 @@
+package br.com.urban.sandbox.jpa;
+
+public class TesteStatistics {
+
+	public static void main(String[] args) {
+		// http://blog.caelum.com.br/cacando-seus-gargalos-com-o-hibernate-statistics/
+	}
+}
