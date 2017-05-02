6ad7fbac1a31d4b224dfaa12b97a42ae9af25709
Diego Leonardo Urban <diego.urban88@gmail.com>
2017-05-02 20:27:28 -0300

    removing

--- src/test/java/br/com/urban/sandbox/jpa/TesteStatistics.java
+++
@@ -1,8 +0,0 @@
-package br.com.urban.sandbox.jpa;
-
-public class TesteStatistics {
-
-	public static void main(String[] args) {
-		// http://blog.caelum.com.br/cacando-seus-gargalos-com-o-hibernate-statistics/
-	}
-}
